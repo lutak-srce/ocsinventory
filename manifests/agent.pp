e0c7fd614ea9f60be0d0ca3a80d04e074749fd5d
Zaviša Nikač <zavisa.nikac@srce.hr>
2017-06-30 16:01:04 +0200

    logging to file

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -1,16 +1,22 @@
 class ocsinventory::agent (
-  String               $package_name,
-  String               $package_ensure,
-  Array                $support_packages,
-  String               $support_packages_ensure,
-  String               $server,
-  Integer              $pause,
-  Stdlib::Absolutepath $ocsinventory_conf_file,
-  String               $ocsinventory_conf_ensure,
-  String               $ocsinventory_conf_owner,
-  String               $ocsinventory_conf_group,
-  String               $ocsinventory_conf_mode,
-  String               $ocsinventory_conf_epp,
+  String                         $package_name,
+  String                         $package_ensure,
+  Array                          $support_packages,
+  String                         $support_packages_ensure,
+  String                         $server,
+  Integer                        $pause,
+  Stdlib::Absolutepath           $ocsinventory_conf_file,
+  String                         $ocsinventory_conf_ensure,
+  String                         $ocsinventory_conf_owner,
+  String                         $ocsinventory_conf_group,
+  String                         $ocsinventory_conf_mode,
+  String                         $ocsinventory_conf_epp,
+  Optional[Stdlib::Absolutepath] $ocsinventory_sysconfig_file,
+  Optional[String]               $ocsinventory_sysconfig_ensure,
+  Optional[String]               $ocsinventory_sysconfig_owner,
+  Optional[String]               $ocsinventory_sysconfig_group,
+  Optional[String]               $ocsinventory_sysconfig_mode,
+  Optional[String]               $ocsinventory_sysconfig_epp,
 ) {
   package { $::ocsinventory::agent::package_name :
     ensure  => $::ocsinventory::agent::package_ensure,
@@ -27,4 +33,18 @@ class ocsinventory::agent (
     group   => $::ocsinventory::agent::ocsinventory_conf_group,
     mode    => $::ocsinventory::agent::ocsinventory_conf_mode,
   }
+
+  case $facts['os']['family'] {
+    'RedHat' : {
+      file { $::ocsinventory::agent::ocsinventory_sysconfig_file:
+        ensure  => $::ocsinventory::agent::ocsinventory_sysconfig_ensure,
+        require => Package[$::ocsinventory::agent::package_name],
+        content => epp($::ocsinventory::agent::ocsinventory_sysconfig_epp),
+        owner   => $::ocsinventory::agent::ocsinventory_sysconfig_owner,
+        group   => $::ocsinventory::agent::ocsinventory_sysconfig_group,
+        mode    => $::ocsinventory::agent::ocsinventory_sysconfig_mode,
+      }
+    }
+  }
+
 }
