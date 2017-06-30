451e9d66859b7d7ab32458464fc9b365b931a8e9
Zaviša Nikač <zavisa.nikac@srce.hr>
2017-06-30 15:02:30 +0200

    add support_packages param & data in modules for srce branch

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -1,23 +1,30 @@
-# Class: ocsinventory::agent
-#
-# This module manages OCS inventory agent
 class ocsinventory::agent (
-  $package_ensure         = $ocsinventory::params::package_ensure,
-  $server                 = $ocsinventory::params::server,
-  $pause                  = $ocsinventory::params::pause,
-  $package_name           = $ocsinventory::params::package_name,
-  $ocsinventory_conf_path = $ocsinventory::params::ocsinventory_conf_path,
-  $ocsinventory_conf_tmpl = $ocsinventory::params::ocsinventory_conf_tmpl,
-) inherits ocsinventory::params {
-  package { $package_name :
-    ensure  => $package_ensure,
+  String               $package_name,
+  String               $package_ensure,
+  Array                $support_packages,
+  String               $support_packages_ensure,
+  String               $server,
+  Integer              $pause,
+  Stdlib::Absolutepath $ocsinventory_conf_file,
+  String               $ocsinventory_conf_ensure,
+  String               $ocsinventory_conf_owner,
+  String               $ocsinventory_conf_group,
+  String               $ocsinventory_conf_mode,
+  String               $ocsinventory_conf_epp,
+) {
+  package { $::ocsinventory::agent::package_name :
+    ensure  => $::ocsinventory::agent::package_ensure,
+    require => Package[$::ocsinventory::agent::support_packages],
   }
-  file { $ocsinventory_conf_path :
-    ensure  => present,
-    require => Package[$package_name],
-    content => template($ocsinventory_conf_tmpl),
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
+  package { $::ocsinventory::agent::support_packages :
+    ensure  => $::ocsinventory::agent::support_packages_ensure,
+  }
+  file { $::ocsinventory::agent::ocsinventory_conf_file:
+    ensure  => $::ocsinventory::agent::ocsinventory_conf_ensure,
+    require => Package[$::ocsinventory::agent::package_name],
+    content => epp($::ocsinventory::agent::ocsinventory_conf_epp),
+    owner   => $::ocsinventory::agent::ocsinventory_conf_owner,
+    group   => $::ocsinventory::agent::ocsinventory_conf_group,
+    mode    => $::ocsinventory::agent::ocsinventory_conf_mode,
   }
 }
